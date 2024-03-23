03869ce4955a788b1309b8266a13c24ef71f2c91
Sahani Amarasinghe <baushiako@gmail.com>
2024-03-24 00:33:50 +0530

    first commit

---
+++ src/app/feature/dashboard-home/dashboard-home.ts
@@ -0,0 +1,37 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DashboardLayoutComponent} from "../../components/dashboard-layout/dashboard-layout.component";
+import {HeaderComponent} from "../../components/header/header.component";
+import {SideNavComponent} from "../../components/side-nav/side-nav.component";
+import {SideNavButtonComponent} from "../../components/side-nav-button/side-nav-button.component";
+import {
+  tuiIconCreditCard,
+  tuiIconFilePlusLarge,
+  tuiIconBarChart,
+  tuiIconGrid,
+  tuiIconUser,
+  tuiIconFilePlus,
+  tuiIconUsersLarge
+} from "@taiga-ui/icons";
+import {DashboardCardsComponent} from "../../components/dashboard-cards/dashboard-cards.component";
+import {MonthlyBudgetCardComponent} from "../../components/monthly-budget-card/monthly-budget-card.component";
+import {MonthlyExpensesCardComponent} from "../../components/monthly-expenses-card/monthly-expenses-card.component";
+import {RouterOutlet} from "@angular/router";
+import {TodayExpensesCardComponent} from "../../components/today-expenses-card/today-expenses-card.component";
+import {TotalExpensesCardComponent} from "../../components/total-expenses-card/total-expenses-card.component";
+
+@Component({
+  selector: 'app-admin-home',
+  standalone: true,
+  imports: [CommonModule, DashboardLayoutComponent, HeaderComponent, SideNavComponent, SideNavButtonComponent, DashboardCardsComponent, MonthlyBudgetCardComponent, MonthlyExpensesCardComponent, RouterOutlet, TodayExpensesCardComponent, TotalExpensesCardComponent],
+  templateUrl: './admin-home.component.html'
+})
+export class AdminHomeComponent {
+  sideNavCollapsed: boolean = true;
+
+  protected readonly tuiIconUsersLarge = tuiIconUsersLarge;
+  protected readonly tuiIconBarChart = tuiIconBarChart;
+  protected readonly tuiIconCreditCard = tuiIconCreditCard;
+  protected readonly tuiIconGrid = tuiIconGrid;
+  protected readonly tuiIconFilePlus = tuiIconFilePlus;
+}
